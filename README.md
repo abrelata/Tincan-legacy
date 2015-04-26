d7aa2031261834cb68a9d4cd33f3598661ba2de9
jamesacuan <jamesacuan@gmail.com>
2015-04-26 21:25:16 +0800

    readded

---
+++ README.md
@@ -0,0 +1,4 @@
+Project Tincan (archived)
+===================================================
+
+Tincan is a mobile application that extends our local telephone company's calling plan to their subscribers' Android device.
---
+++ src/com/tincan/tincan/CallLogObject.java
@@ -0,0 +1,18 @@
+package com.tincan.tincan;
+
+public class CallLogObject {
+	private String contact, duration;
+	
+	public void setContact(String contact){
+		this.contact = contact;
+	}
+	public void setDuration(String duration){
+		this.duration = duration;
+	}
+	public String getContact(){
+		return contact;
+	}
+	public String getDuration(){
+		return duration;
+	}
+}
